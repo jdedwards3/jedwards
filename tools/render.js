e5b20ca8a6f32ea5c0c1dd2c6ca3d7d350274688
jdedwards3 <james.d.edwards.3@gmail.com>
2019-02-18 20:49:03 -0500

    adding minimal ejs rendering script

---
+++ tools/render.js
@@ -0,0 +1,20 @@
+const ejs = require("ejs");
+const fs = require("fs");
+
+ejs.renderFile("views/index.ejs", function(err, html) {
+  if (err) {
+    throw err;
+  }
+
+  fs.mkdir("built/", { recursive: true }, function(err) {
+    if (err) {
+      throw err;
+    }
+
+    fs.writeFile("built/index.html", html, "utf8", function(err) {
+      if (err) {
+        throw err;
+      }
+    });
+  });
+});
