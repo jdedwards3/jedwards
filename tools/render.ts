63fd6161b1302e1c6934e29c2f4654b3e46880c6
jdedwards3 <james.d.edwards.3@gmail.com>
2019-02-20 23:39:57 -0500

    rendering partial view in master layout

--- tools/render.ts
+++ tools/render.ts
@@ -1,20 +1,30 @@
 import ejs = require("ejs");
 import fs = require("fs");
 
-ejs.renderFile("views/index.ejs", function(err, html) {
+ejs.renderFile("views/pages/index.ejs", function(err, partialHtml) {
   if (err) {
     throw err;
   }
-
-  fs.mkdir("built/", { recursive: true }, function(err) {
-    if (err) {
-      throw err;
-    }
-
-    fs.writeFile("built/index.html", html, "utf8", function(err) {
+  ejs.renderFile(
+    "views/index.ejs",
+    { main: partialHtml },
+    { rmWhitespace: true },
+    function(err, html) {
       if (err) {
         throw err;
       }
-    });
-  });
+
+      fs.mkdir("built/", { recursive: true }, function(err) {
+        if (err) {
+          throw err;
+        }
+
+        fs.writeFile("built/index.html", html, "utf8", function(err) {
+          if (err) {
+            throw err;
+          }
+        });
+      });
+    }
+  );
 });
