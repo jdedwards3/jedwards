b88eb6eb4e2d99ac1a24c0bc20319d5671ea858a
jdedwards3 <james.d.edwards.3@gmail.com>
2019-03-02 13:37:49 -0500

    updating build to render files dynamically

--- tools/render.ts
+++ tools/render.ts
@@ -1,30 +1,43 @@
 import ejs = require("ejs");
 import fs = require("fs");
+import globstd = require("glob");
+import util = require("util");
+const glob = util.promisify(globstd);
+const mkdir = util.promisify(fs.mkdir);
+const writeFile = util.promisify(fs.writeFile);
 
-ejs.renderFile("views/pages/index.ejs", function(err, partialHtml) {
-  if (err) {
-    throw err;
-  }
-  ejs.renderFile(
-    "views/index.ejs",
-    { main: partialHtml },
-    { rmWhitespace: true },
-    function(err, html) {
-      if (err) {
-        throw err;
-      }
+const viewsPath = "./views";
 
-      fs.mkdir("built/", { recursive: true }, function(err) {
-        if (err) {
-          throw err;
-        }
+(async function initialize() {
+  const [indexPath, filePaths]: [string[], string[]] = await Promise.all([
+    glob("index.ejs", { cwd: `${viewsPath}` }),
+    glob("**/*.ejs", {
+      cwd: `${viewsPath}`,
+      ignore: ["partials/**/*.ejs", "index.ejs", "layout.ejs"]
+    })
+  ]).then(paths => paths);
 
-        fs.writeFile("built/index.html", html, "utf8", function(err) {
-          if (err) {
-            throw err;
-          }
-        });
-      });
-    }
+  await mkdir("built/", { recursive: true });
+
+  await Promise.all(
+    filePaths.map(async path => {
+      const partialHtml = await ejs
+        .renderFile(`${viewsPath}/${path}`)
+        .then(output => output);
+
+      const renderedFile = await ejs
+        .renderFile(
+          `${viewsPath}/${indexPath[0]}`,
+          { main: partialHtml },
+          { rmWhitespace: true }
+        )
+        .then(output => output);
+
+      await writeFile(
+        `built/${path.split("/")[1].split(".")[0]}.html`,
+        renderedFile,
+        "utf8"
+      );
+    })
   );
-});
+})();
