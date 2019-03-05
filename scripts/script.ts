c21e59cbe257dbd0c8c70283a29494695c916476
jdedwards3 <james.d.edwards.3@gmail.com>
2019-03-05 00:30:06 -0500

    test client side script

---
+++ scripts/script.ts
@@ -0,0 +1,18 @@
+console.log("test");
+
+// $("a").click(function(event: JQuery.Event) {
+//   event.preventDefault();
+//   const self = $(this);
+//   console.log(self);
+
+//   fetch(`api/${self.attr("href")}.json`)
+//     .then(function(response) {
+//       return response.json();
+//     })
+//     .then(function(data) {
+//       console.log(data);
+
+//       $("main").html(data.partialHtml);
+//       $("title").text(data.title);
+//     });
+// });
