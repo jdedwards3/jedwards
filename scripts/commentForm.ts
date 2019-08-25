1da0a18669355a65f6d661d6e7e0de8309f1b51f
jdedwards3 <james.d.edwards.3@gmail.com>
2019-08-25 12:23:28 -0400

    adding message during form submit

--- scripts/commentForm.ts
+++ scripts/commentForm.ts
@@ -4,6 +4,9 @@ export class CommentForm {
   constructor() {
     document.body.addEventListener("submit", async function(event) {
       event.preventDefault();
+      const statusMsgElement = document.getElementById("form-submit-msg");
+
+      statusMsgElement!.innerText = "Submitting reply... Please wait.";
 
       const form = event.target as HTMLFormElement;
 
@@ -20,9 +23,7 @@ export class CommentForm {
         .then(json => json)
         .catch(errorLogger);
 
-      const resultMsg = document.getElementById("form-submit-msg");
-
-      resultMsg!.innerText = result.message;
+      statusMsgElement!.innerText = result.message;
     });
   }
 }
