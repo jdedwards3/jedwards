7dbb16fdec081a4b65a659d8f955a6b34204362b
jdedwards3 <james.d.edwards.3@gmail.com>
2020-05-10 13:24:09 -0400

    version 1.6.3

--- config.ts
+++ config.ts
@@ -11,5 +11,5 @@ export const config: any = {
     domain: "https://www.jamesedwards.name",
     functionsUrl: "https://jedwards.azurewebsites.net/api",
   },
-  version: "1.6.2",
+  version: "1.6.3",
 };
