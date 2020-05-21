211f70cec4a8980ad84921ca80df5d9119a1037a
jdedwards3 <james.d.edwards.3@gmail.com>
2020-05-20 21:45:06 -0400

    version 1.7.0

--- config.ts
+++ config.ts
@@ -11,5 +11,5 @@ export const config: any = {
     domain: "https://www.jamesedwards.name",
     functionsUrl: "https://jedwards.azurewebsites.net/api",
   },
-  version: "1.6.5",
+  version: "1.7.0",
 };
