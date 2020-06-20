2d5057819dd41dfce691b223499b2aa6d5b7fa66
jdedwards3 <james.d.edwards.3@gmail.com>
2020-06-20 19:58:16 -0400

    only load ad reload if showing ads

--- scripts/emptyAdReloader.ts
+++ scripts/emptyAdReloader.ts
@@ -1,17 +1,9 @@
 export class EmptyAdReloader {
-  constructor() {
-    // show previously hidden ads on window resize
-    window.onresize = function () {
-      const emptyAds = Array.from(document.querySelectorAll("ins")).filter(
-        function (elem) {
-          return !elem.children.length && elem.offsetHeight > 0;
-        }
-      );
-      emptyAds.forEach(function (elem) {
-        delete elem.dataset.adsbygoogleStatus;
-        let adsbygoogle = (window as any).adsbygoogle || [];
-        adsbygoogle.push({});
-      });
-    };
+  constructor(emptyAds: HTMLModElement[]) {
+    emptyAds.forEach(function (elem) {
+      delete elem.dataset.adsbygoogleStatus;
+      let adsbygoogle = (window as any).adsbygoogle || [];
+      adsbygoogle.push({});
+    });
   }
 }
--- scripts/script.ts
+++ scripts/script.ts
@@ -1,8 +1,19 @@
-import { EmptyAdReloader } from "./emptyAdReloader.js";
 window.onload = async function () {
-  new EmptyAdReloader();
   if (document.querySelectorAll("form[data-type]").length > 0) {
     const { FormHandler } = await import("./formHandler.js");
     new FormHandler();
+  }
+};
+
+window.onresize = async function () {
+  // show previously hidden ads on window resize
+  const emptyAds = Array.from(document.querySelectorAll("ins")).filter(
+    function (elem) {
+      return !elem.children.length && elem.offsetHeight > 0;
+    }
+  );
+  if (emptyAds) {
+    const { EmptyAdReloader } = await import("./emptyAdReloader.js");
+    new EmptyAdReloader(emptyAds);
   }
 };
