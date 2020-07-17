261c3dc1ac6a52a06af6bd02c989845b12fb9270
jdedwards3 <james.d.edwards.3@gmail.com>
2020-07-16 22:17:10 -0400

    remove ad reloading code

--- scripts/emptyAdReloader.ts
+++
@@ -1,8 +0,0 @@
-const emptyAdReloader = (emptyAds: HTMLModElement[]) => {
-  emptyAds.forEach(function (elem) {
-    delete elem.dataset.adsbygoogleStatus;
-    let adsbygoogle = (window as any).adsbygoogle || [];
-    adsbygoogle.push({});
-  });
-};
-export { emptyAdReloader };
--- scripts/script.ts
+++ scripts/script.ts
@@ -4,16 +4,3 @@ window.onload = async function () {
     new FormHandler();
   }
 };
-
-window.onresize = async function () {
-  // show previously hidden ads on window resize
-  const emptyAds = Array.from(document.querySelectorAll("ins")).filter(
-    function (elem) {
-      return !elem.children.length && elem.offsetHeight > 0;
-    }
-  );
-  if (emptyAds.length) {
-    const { emptyAdReloader } = await import("./emptyAdReloader.js");
-    emptyAdReloader(emptyAds);
-  }
-};
