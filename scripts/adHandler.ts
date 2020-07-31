5fb9b98a38f6cd77c87c467bce9d8ea0e27e770b
jdedwards3 <james.d.edwards.3@gmail.com>
2020-07-30 20:05:38 -0400

    restore conditional display

---
+++ scripts/adHandler.ts
@@ -0,0 +1,33 @@
+declare var ad1: any, ad2: any, ad3: any, ad4: any, ad5: any, ad6: any;
+
+const adCheck = function () {
+  const w = window as any;
+  const googletag = w.googletag;
+
+  if (window.innerWidth > 767) {
+    googletag.cmd.push(function () {
+      googletag.pubads().refresh([ad1, ad4, ad5]);
+    });
+  } else {
+    googletag.cmd.push(function () {
+      googletag.pubads().refresh([ad2, ad3, ad6]);
+    });
+  }
+};
+
+const adReload = function (t: any) {
+  const googletag = (window as any).googletag as any;
+
+  googletag.cmd.push(function () {
+    googletag.pubads().clear();
+  });
+
+  clearTimeout(t);
+  t = setTimeout(() => {
+    adCheck();
+  }, 1500);
+
+  return t;
+};
+
+export { adCheck, adReload };
--- scripts/script.ts
+++ scripts/script.ts
@@ -1,6 +1,20 @@
-window.onload = async function () {
-  if (document.querySelectorAll("form[data-type]").length) {
-    const { FormHandler } = await import("./formHandler.js");
-    new FormHandler();
+(async function () {
+  window.addEventListener("load", async function () {
+    if (document.querySelectorAll("form[data-type]").length) {
+      const { FormHandler } = await import("./formHandler.js");
+      new FormHandler();
+    }
+    if (document.querySelectorAll(".ad-unit").length) {
+      const { adCheck } = await import("./adHandler.js");
+      adCheck();
+    }
+  });
+
+  let t: any = null;
+  if (document.querySelectorAll(".ad-unit").length) {
+    const { adReload } = await import("./adHandler.js");
+    window.addEventListener("resize", function () {
+      t = adReload(t);
+    });
   }
-};
+})();
