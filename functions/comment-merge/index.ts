2d350348210957f3d3943e77a752d8328d70bb5e
jdedwards3 <james.d.edwards.3@gmail.com>
2020-08-31 23:01:58 -0400

    adding comment repo store

---
+++ functions/comment-merge/index.ts
@@ -0,0 +1,115 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import util = require("util");
+import * as querystring from "querystring";
+import * as simpleGit from "simple-git/promise";
+import fs = require("fs");
+import { tmpdir } from "os";
+import uuidv4 = require("uuid/v4");
+import globstd = require("glob");
+import rimrafstd = require("rimraf");
+const rimraf = util.promisify(rimrafstd);
+const glob = util.promisify(globstd);
+const mkdir = util.promisify(fs.mkdir);
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  context.log("HTTP trigger function processed a request.");
+
+  context.res!.headers["Content-Type"] = "application/json";
+
+  const payload = JSON.parse(querystring.parse(req.body).payload as string);
+
+  //todo: validate header secret
+  if (payload.action != "closed") {
+    return;
+  }
+
+  const tempRepo = uuidv4();
+
+  await mkdir(`${tmpdir}/${tempRepo}/viewData/comments`, {
+    recursive: true,
+  });
+
+  const git = simpleGit(`${tmpdir}/${tempRepo}`);
+
+  await git.init();
+
+  await Promise.all([
+    git.addConfig("user.name", `${process.env["GitHubUser"]}`),
+    git.addConfig("user.email", `${process.env["AdminEmail"]}`),
+  ]);
+
+  await Promise.all([
+    git.addRemote(
+      "private",
+      `https://${process.env["GitHubUser"]}:${process.env["GitHubUserPassword"]}@${process.env["GitHubPrivateRepo"]}`
+    ),
+    git.addRemote(
+      "public",
+      `https://${process.env["GitHubUser"]}:${process.env["GitHubUserPassword"]}@${process.env["GitHubPublicRepo"]}`
+    ),
+  ]);
+
+  await git.fetch("public", `${process.env["BaseBranch"]}`);
+
+  await git.checkout(`public/${process.env["BaseBranch"]}`, [
+    "--",
+    "viewData/comments/",
+  ]);
+
+  await git.checkoutBranch(
+    `${process.env["BaseBranch"]}`,
+    `public/${process.env["BaseBranch"]}`
+  );
+
+  await git.fetch("private", `${process.env["BaseBranch"]}`);
+
+  await git.checkout(`private/${process.env["BaseBranch"]}`, [
+    "--",
+    "viewData/comments/",
+  ]);
+
+  const paths = await glob(`comments/**/*.json`, {
+    cwd: `${tmpdir}/${tempRepo}/viewData/`,
+  });
+
+  await Promise.all(
+    paths.map(async (path) => {
+      let pathData = [];
+
+      pathData = [
+        ...JSON.parse(
+          await readFile(`${tmpdir}/${tempRepo}/viewData/${path}`, "utf8")
+        ),
+      ];
+
+      const publicData = pathData.map((item) => {
+        const { authorEmail, ...store } = item;
+        return store;
+      });
+
+      await writeFile(
+        `${tmpdir}/${tempRepo}/viewData/${path}`,
+        JSON.stringify(publicData, null, 2),
+        "utf8"
+      );
+    })
+  );
+
+  await git.add(`${tmpdir}/${tempRepo}/viewData/comments/*.json`);
+
+  await git.commit("approving comment");
+
+  await git.push("public", `${process.env["BaseBranch"]}`);
+
+  await rimraf(`${tmpdir}/${tempRepo}/`);
+
+  context.res!.status = 200;
+  context.res!.body = { message: "success" };
+};
+
+export default httpTrigger;
--- functions/comment/index.ts
+++ functions/comment/index.ts
@@ -1,13 +1,23 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions";
 import * as querystring from "querystring";
+import util = require("util");
 import uuidv4 = require("uuid/v4");
 import * as SendGrid from "@sendgrid/mail";
+import * as simpleGit from "simple-git/promise";
 import { storageHelpers } from "../common/storageHelpers";
 import { formHelpers } from "../common/formHelpers";
+import { Octokit } from "@octokit/rest";
+import fs = require("fs");
+import rimrafstd = require("rimraf");
+import { tmpdir } from "os";
+const rimraf = util.promisify(rimrafstd);
+const mkdir = util.promisify(fs.mkdir);
+const writeFile = util.promisify(fs.writeFile);
+const readFile = util.promisify(fs.readFile);
 SendGrid.setApiKey(process.env["SendGridApiKey"] as string);
 const commentTable = "comments";
 
-const httpTrigger: AzureFunction = async function(
+const httpTrigger: AzureFunction = async function (
   context: Context,
   req: HttpRequest
 ): Promise<void> {
@@ -32,47 +42,133 @@ const httpTrigger: AzureFunction = async function(
       body.authorEmail &&
       body.authorName
     ) {
-      await storageHelpers.createTableIfNotExists(commentTable);
+      const tempRepo = uuidv4();
 
+      await mkdir(`${tmpdir}/${tempRepo}/viewData/comments`, {
+        recursive: true,
+      });
+
+      const git = simpleGit(`${tmpdir}/${tempRepo}`);
+
+      await git.init();
+
+      await git.addConfig("user.name", `${process.env["GitHubUser"]}`);
+      await git.addConfig("user.email", `${process.env["AdminEmail"]}`);
+
+      await git.addRemote(
+        "private",
+        `https://${process.env["GitHubUser"]}:${process.env["GitHubUserPassword"]}@${process.env["GitHubPrivateRepo"]}`
+      );
+
+      const commentId = uuidv4();
+
+      try {
+        await git.fetch("private", `${process.env["BaseBranch"]}`);
+
+        await git.checkout(`private/${process.env["BaseBranch"]}`, [
+          "--",
+          `viewData/comments/${body.postGuid}.json`,
+        ]);
+
+        await git.checkoutBranch(
+          `${commentId}`,
+          `private/${process.env["BaseBranch"]}`
+        );
+      } catch (error) {
+        await git.checkout(`private/${process.env["BaseBranch"]}`);
+        await git.checkoutLocalBranch(`${commentId}`);
+      }
+
+      const comment = {
+        id: commentId,
+        timestamp: new Date(new Date().toUTCString()).getTime(),
+        authorEmail: body.authorEmail,
+        authorName: body.authorName,
+        bodyText: body.comment,
+      };
+
+      let comments = [];
+
+      try {
+        comments = JSON.parse(
+          await readFile(
+            `${tmpdir}/${tempRepo}/viewData/comments/${body.postGuid}.json`,
+            "utf8"
+          )
+        );
+      } catch (error) {
+        //no previous comments
+      }
+
+      comments.push(comment);
+
+      await writeFile(
+        `${tmpdir}/${tempRepo}/viewData/comments/${body.postGuid}.json`,
+        JSON.stringify(comments, null, 2),
+        "utf8"
+      );
+
+      await git.add(
+        `${tmpdir}/${tempRepo}/viewData/comments/${body.postGuid}.json`
+      );
+
+      await git.commit(`adding comment ${commentId}`);
+
+      await git.push("private", `${commentId}`);
+
+      await new Octokit({
+        auth: process.env["GitHubUserPassword"],
+      }).pulls.create({
+        owner: `${process.env["GitHubUser"]}`,
+        repo: `${process.env["PrivateRepoName"]}`,
+        title: `${commentId}`,
+        head: `${commentId}`,
+        base: `${process.env["BaseBranch"]}`,
+      });
+
+      await rimraf(`${tmpdir}/${tempRepo}/`);
+
+      /* todo: remove */
+      await storageHelpers.createTableIfNotExists(commentTable);
       const commentEntity = {
         PartitionKey: body.postGuid,
         RowKey: uuidv4(),
         status: 0,
         authorEmail: body.authorEmail,
         authorName: body.authorName,
-        bodyText: body.comment
+        bodyText: body.comment,
       };
-
       await storageHelpers.insertEntity(commentTable, commentEntity);
+      /* end remove */
 
       const userEmail = {
         to: body.authorEmail,
         from: "noreply@jamesedwards.name",
         subject: "Thank you for your comment!",
-        text: "It will be posted when approved."
+        text: "It will be posted when approved.",
       };
 
       const adminEmail = {
         to: process.env["AdminEmail"],
         from: "noreply@jamesedwards.name",
         subject: "New comment posted!",
-        html: `A new comment has been posted. 
+        html: `A new comment has been posted.
         <div>from: ${body.authorName}</div>
         <div>email: ${body.authorEmail}</div>
         <div>comment: ${body.comment}</div>
-        Update status to approve.`
+        Update status to approve.`,
       };
 
       await Promise.all([SendGrid.send(userEmail), SendGrid.send(adminEmail)]);
 
       context.res!.status = 200;
       context.res!.body = {
-        message: "Thank you for your comment. It will be posted when approved."
+        message: "Thank you for your comment. It will be posted when approved.",
       };
     } else {
       context.res!.status = 400;
       context.res!.body = {
-        message: "Comment invalid. Please correct errors and try again."
+        message: "Comment invalid. Please correct errors and try again.",
       };
     }
   } else if (req.method == "GET") {
@@ -93,7 +189,7 @@ const httpTrigger: AzureFunction = async function(
   } else {
     context.res!.status = 500;
     context.res!.body = {
-      message: "An error occured. Please try again later."
+      message: "An error occured. Please try again later.",
     };
   }
 };
