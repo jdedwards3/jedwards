82c5eb2f75f601125344c3c4212926725a3c7e3d
jdedwards3 <james.d.edwards.3@gmail.com>
2021-01-09 18:40:59 -0500

    add service worker

---
+++ content/pages/offline.md
@@ -0,0 +1,7 @@
+---
+title: You Are Not Connected to the Internet
+metaDescription: <%= config.siteDescription %>
+guid: e5d13960-9eed-415c-94bb-386f3c72c054
+---
+
+[reload page]()
--- scripts/script.ts
+++ scripts/script.ts
@@ -1,4 +1,11 @@
 (async function () {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/service-worker.js", { scope: "/" })
+      .then(function () {
+        console.log("Service Worker Registered");
+      });
+  }
   window.addEventListener("load", async function () {
     if (document.querySelectorAll("form[data-type]").length) {
       const { FormHandler } = await import("./formHandler.js");
